06f89bcb2ad4af904fb65e381878881adf478d43
Ríona Stokes <rlsted123@gmail.com>
2021-06-10 18:19:29 +0200

    Update to comments of script

--- script/search.js
+++ script/search.js
@@ -3,7 +3,7 @@ const Fetch = require('node-fetch')
 
 
 async function doSearch() {
-  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1')
+  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1&per_page=5')
   const body = await response.text()
   const json = JSON.parse(body)
   console.log('TOTAL', json.total_count)
@@ -16,10 +16,23 @@ async function doSearch() {
     desc: item.description,
     // type: item.foo,
     update: item.updated_at,
+
+/**
+ * TESTS to tell apart items (the what)
+ * 
+ * homepage: item.homepage,
+ * mirror: item.mirror_url,
+ * Not gonna work --> Scan text in README to find mention of string "plugin"=true ? Include version with/without hyphen, capitals, etc (unnecs complex?)
+ * Top-down appraoch => figure this out later
+ *    
+ */
+
+
   }))
 
   console.log(summary)
-
+  // NEXT STEP: test saving them to file as json blob (or directly as csv?)
+  
   // console.log(json)
 }
 
@@ -31,7 +44,9 @@ async function doSearch() {
  * Name :  item.name
  * Description : item.description
  * What (core/plugin/support) : 
- * lastUpdate : item.updated_at
+ * lastUpdate : item.updated_at (take the first 10
+ *              characters of the string to match 
+ *              date format)
  */
 
 doSearch()
