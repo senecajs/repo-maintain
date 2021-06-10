2a2579857f6fc57080a54bb8af7b094efa75c3b1
Ríona Stokes <rlsted123@gmail.com>
2021-06-10 16:46:52 +0200

    Search results with catalog headings
    First 30 search results excluding type/what heading

--- script/search.js
+++ script/search.js
@@ -3,20 +3,24 @@ const Fetch = require('node-fetch')
 
 
 async function doSearch() {
-  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1&per_page=3')
+  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1')
   const body = await response.text()
   const json = JSON.parse(body)
   console.log('TOTAL', json.total_count)
   console.log('ITEMS LEN', json.items.length)
 
-  // let summary = json.items.map(item=>({
-  //   name: item.name,
-  //   owner: item.owner.login,
-  // }))
+  let summary = json.items.map(item=>({
+    repo: item.html_url,
+    owner: item.owner.login,
+    name: item.name,
+    desc: item.description,
+    // type: item.foo,
+    update: item.updated_at,
+  }))
 
-  // console.log(summary)
+  console.log(summary)
 
-  console.log(json)
+  // console.log(json)
 }
 
 /**
