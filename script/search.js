3ffb8bdb2d6cfa7a10a315a72fbb575f474a335b
Ríona Stokes <rlsted123@gmail.com>
2021-06-11 10:33:30 +0200

    Require fs and brainstorming in comments

--- script/search.js
+++ script/search.js
@@ -1,9 +1,28 @@
 
 const Fetch = require('node-fetch')
+const fs = require('fs')  // invite file system module to script
+
+/**
+ * HOW to insert data to spreadsheet?
+ * 1) Automate collecting data as json blob, save as file
+ *    using fs, transfer to csv format by hand (using online
+ *    converter), upload and import manually to google sheets
+ *    using Sheetgo add-on
+ * 2) Automate collecting data as json blob, automate transfer
+ *    to csv format within script (using vscode extensions/
+ *    node modules?), save as file using fs, upload and import
+ *    manually to google sheets
+ * 3) Automate collecting data as csv, save file using fs,
+ *    upload and import manually to google sheets
+ * 
+ * => Solution 3 might be the fastest/least hassle
+ * a) create manual csv string (headings)
+ * 
+ */
 
 
 async function doSearch() {
-  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1&per_page=5')
+  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1&per_page=3')
   const body = await response.text()
   const json = JSON.parse(body)
   console.log('TOTAL', json.total_count)
@@ -14,11 +33,11 @@ async function doSearch() {
     owner: item.owner.login,
     name: item.name,
     desc: item.description,
-    // type: item.foo,
+    // type: item.foo, // still need to figure out how to get this
     update: item.updated_at,
 
 /**
- * TESTS to tell apart items (the what)
+ * TESTS to tell apart items (the what - core/plugin/support)
  * 
  * homepage: item.homepage,
  * mirror: item.mirror_url,
@@ -37,7 +56,7 @@ async function doSearch() {
 }
 
 /**
- * HEADINGS for catalogue
+ * HEADINGS for catalogue (just a reference)
  * 
  * Repo (url) : item.html_url
  * Owner : item.owner.login
