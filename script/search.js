c186940a9322ac6c7283f3ea7276ca15b931a94a
Ríona Stokes <rlsted123@gmail.com>
2021-06-15 18:29:07 +0200

    Creation of Sheets API quickstart + update README

--- script/search.js
+++ script/search.js
@@ -2,7 +2,7 @@ const Fetch = require('node-fetch')
 const fs = require('fs')  // invite file system module to script
 
 async function doSearch() {
-  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1&per_page=3')
+  const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1&per_page=25')
   const body = await response.text()
   const json = JSON.parse(body)
   console.log('TOTAL', json.total_count)
@@ -15,6 +15,9 @@ async function doSearch() {
   let dataToSave = ""
 
   json.items.forEach(repo => {
+
+    // if clauses here to discount non-SenecaJS repos
+
     let url = repo["html_url"]
     let owner = repo["owner"]["login"]
     let name = repo["name"]
