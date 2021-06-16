7d9fbc1c7c3a9602547bef2be23108751f581ae0
Ríona Stokes <rlsted123@gmail.com>
2021-06-16 14:57:30 +0200

    Creating Sheets API constants

--- script/search.js
+++ script/search.js
@@ -1,6 +1,7 @@
 const Fetch = require('node-fetch')
 const fs = require('fs')  // invite file system module to script
 
+
 async function doSearch() {
   const response = await Fetch('https://api.github.com/search/repositories?q=seneca-&page=1&per_page=10')
   const body = await response.text()
@@ -13,10 +14,19 @@ async function doSearch() {
   fs.writeFileSync("../csv/testing/initialTest.csv", headings)
  
   let dataToSave = ""
+  // let relevant = empty json blob (?)
 
-  json.items.forEach(repo => {
+  /**
+   * "if" clauses here only deal with SenecaJS repos
+   * json.items.forEach(maybe => {
+   *  if (maybe["owner"]["login"] == "senecajs"){
+   *   add maybe to relevant (formatted as json)
+   *  }
+   *  if x then y etc etc
+   * })
+   */
 
-    // if clauses here to discount non-SenecaJS repos
+  json.items.forEach(repo => { // replace json.items with relevant
 
     let url = repo["html_url"]
     let owner = repo["owner"]["login"]
@@ -34,9 +44,24 @@ async function doSearch() {
   });
 
   console.log("See ../csv/testing/initialTest.csv for printed values.")
+}
 
+// Google Sheets API - specific requirements
+const readline = require('readline')
+const {google} = require('googleapis')
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+function doSheets(){
+  // send file to google sheets using API
+  // Cloud function -> node js ?
 }
 
 doSearch()
-
+//doSheets()
 
