3126b925dc903abd4b1674718b52594d49be3d25
Ríona Stokes <rlsted123@gmail.com>
2021-06-23 16:13:29 +0200

    Bug testing (re: search API rate/query limit)

--- script/search.js
+++ script/search.js
@@ -21,26 +21,32 @@ if(Fs.existsSync(results)){
 async function doSearch() {
   console.log("Search function initiated.")
 
-  await new Promise(resolve => setTimeout(resolve,1111))
+  
   
   // console.log("Links: " + searchURL.link)
   let logged = 0
   let results = []
   // exit clause for for-loop (once all results have been logged) ?
   // data for # pages held in native code
-  for (let page = 1; page < 11; page++) {
+  for (let page = 1; page < 50; page++) {
+    await new Promise(resolve => setTimeout(resolve,9999))
     let searchURL = "https://api.github.com/search/repositories?q=seneca-&page=" + page.toString() + "&per_page=100"
     const response = await Fetch(searchURL)
+    const ok = response.ok
     const body = await response.text()
     const json = JSON.parse(body)
-    
+    if(null == json.items) {
+      console.log("BAD!!! :(", json)
+    }
+
     // undefined coming up for page 11 in loop of 100 (page 10 for loop of 50)
     // Now it's page 10 ??
-    console.log("[" + page + "] " + json.items.length + " results fetched...")
+    console.log(ok, " [" + page + "] " + json.items.length + " results fetched...")
     logged += json.items.length
     
     json.items.forEach(item => {
       results.push(item)
+      // map full_name = item (object.keys for list)
     });
     
     // append to file - not overwrite (taking for loop into account...)
@@ -49,7 +55,7 @@ async function doSearch() {
     // console.log(json.items)
   }
 
-  Fs.appendFileSync("../data/json/results.json", JSON.stringify(results))
+  Fs.writeFileSync("../data/json/results.json", JSON.stringify(results))
   console.log("Object logged as JSON data.")
 
   console.log("Search completed. See results.json file for logged data.")
--- script/tests/foo.js
+++ script/tests/foo.js
@@ -1,5 +1,3 @@
-console.log('FOO')
-
-const Curl = require('curlconverter')
-
-Curl.toJavaScript("curl -I 'https://api.github.com/search/repositories?q=seneca-'")
+let a = {b:1}
+a = undefined
+console.log(a.b)
