94d4647fd6c3a8bf4263da2f0282e6cb7a091d2f
Ríona Stokes <rlsted123@gmail.com>
2021-06-28 11:04:24 +0100

    Refactoring search.js for simple authentication

--- script/search.js
+++ script/search.js
@@ -1,13 +1,9 @@
 const Fetch = require('node-fetch')
 const Fs = require('fs')  // invite file system module to script
 
-// dependencies for auth
-const Axios = require ('axios')
-const Express = require ('express')
-const App = express()
-// consts for auth
-const clientID = '10324b11c94236aa4b99'
-const clientSecret = '541bd55a0f7a54c715218c0ade437edb595518d3'
+// dependencies for search
+const searchAPI = require ('github-search-api')
+const Github = new searchAPI({username: 'stokesriona', password: 'M0shim0nsteRS182!'})
 
 // get current year
 const today = new Date()
@@ -22,19 +18,21 @@ if(Fs.existsSync(results)){
 
 async function doSearch() {
   console.log("Search function initiated.")
-  // one authentication request for function here?
 
   const map = {}
   // increment year to search
   for (let year = 2010; year <= 2019; year++) { // year <= thisYear; year++) {
     // increment page of search results
-    for (let page = 1; page <= 50; page++) {
+    for (let page = 1; page <= 5; page++) {
 
       await new Promise(resolve => setTimeout(resolve,1111))
 
-      // or one authentication request per search?
-      let searchURL = "https://api.github.com/search/repositories?q=seneca-+created:%3C" + year.toString() + "-01-01&page=" + page.toString() + "&per_page=100"
-      const response = await Fetch(searchURL)
+      let searchURL = "seneca-+created:%3C" + year.toString() + "-01-01&page=" + page.toString() + "&per_page=100"
+      Github.searchRepos(searchURL, function(data){
+        console.log(data)
+        const response = data
+      })
+
       const body = await response.text()
       const json = JSON.parse(body)
       // catching error
@@ -56,6 +54,8 @@ async function doSearch() {
       if(json.items.length <= 100){
         break
       }
+
+      
     }
 
     
