e1de22676db4af110c0583c9c8d62e3229ce160c
Ríona Stokes <rlsted123@gmail.com>
2021-06-28 10:24:02 +0100

    Including auth dependencies in search.js

--- script/search.js
+++ script/search.js
@@ -1,6 +1,14 @@
 const Fetch = require('node-fetch')
 const Fs = require('fs')  // invite file system module to script
 
+// dependencies for auth
+const Axios = require ('axios')
+const Express = require ('express')
+const App = express()
+// consts for auth
+const clientID = '10324b11c94236aa4b99'
+const clientSecret = '541bd55a0f7a54c715218c0ade437edb595518d3'
+
 // get current year
 const today = new Date()
 const thisYear = today.getFullYear()
@@ -14,6 +22,7 @@ if(Fs.existsSync(results)){
 
 async function doSearch() {
   console.log("Search function initiated.")
+  // one authentication request for function here?
 
   const map = {}
   // increment year to search
@@ -23,6 +32,7 @@ async function doSearch() {
 
       await new Promise(resolve => setTimeout(resolve,1111))
 
+      // or one authentication request per search?
       let searchURL = "https://api.github.com/search/repositories?q=seneca-+created:%3C" + year.toString() + "-01-01&page=" + page.toString() + "&per_page=100"
       const response = await Fetch(searchURL)
       const body = await response.text()
