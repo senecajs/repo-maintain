2652f120d85bb05045eb6d6d6acf7d5d619d5209
Ríona Stokes <rlsted123@gmail.com>
2021-07-02 11:05:05 +0100

    Console log of repo names from sheets using api

--- script/upload.js
+++ script/upload.js
@@ -16,7 +16,7 @@ const TOKEN_PATH = '../data/json/token.json';
 Fs.readFile('../data/json/sheetsCredentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), uploadCSV);
+  authorize(JSON.parse(content), listMajors);
 });
 
 /**
@@ -72,42 +72,22 @@ function getNewToken(oAuth2Client, callback) {
 /**
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function uploadCSV(auth){
-    // upload to existing sheet
-    // modify single cell on repo sheet
-    console.log("hewwo?")
-    const sheets = google.sheets({version: 'v4', auth});
-    console.log("uwuu")
-    return new Promise((resolve, reject) => {
-        const emptySheetParams = {
-            spreadsheetId: "1vgL0ZiL1A33h0RANncoQBgliwFkdcNlyyoCDvr2aioY",
-            resource: {
-                requests: [
-                    {
-                        addSheet: {
-                            properties: {
-                                    title: "totalResults",
-                                    index: 1,
-                                    gridProperties: {
-                                    rowCount: 2000,
-                                    columnCount: 2000,
-                                    frozenRowCount: 1
-                                }
-                            }
-                        }
-                    }
-                ]
-            }
-        }
-        sheets.spreadsheets.batchUpdate(emptySheetParams, function(err, response) {
-            if (err) {
-            reject("The Sheets API returned an error: " + err)
-            } else {
-            const sheetId = response.data.replies[0].addSheet.properties.sheetId
-            console.log("Created empty sheet: " + sheetId)
-            resolve(sheetId)
-            }
-        }
-        );
-    });
+ function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1vgL0ZiL1A33h0RANncoQBgliwFkdcNlyyoCDvr2aioY',
+    range: 'repo!A2:F15',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log('Name:');
+      // Print columns A and E, which correspond to indices 0 and 4.
+      rows.map((row) => {
+        console.log(`${row[2]}`);
+      });
+    } else {
+      console.log('No data found.');
+    }
+  });
 }
