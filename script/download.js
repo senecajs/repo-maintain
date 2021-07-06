d6d7fb708e0387cfda9e1ac3aa107b22247b7686
Ríona Stokes <rlsted123@gmail.com>
2021-07-06 22:37:25 +0100

    Initial version download.js script

---
+++ script/download.js
@@ -0,0 +1,27 @@
+const Fetch = require('node-fetch')
+const Fs = require('fs')
+const Plugins = require('../data/json/plugins.json')
+
+async function doDownload() {
+    console.log("Download function initiated.")
+
+    list = JSON.parse(Plugins)
+    list.forEach(repo => {
+        console.log(repo.key())
+    });
+
+    // let readmeURL = "https://raw.githubusercontent.com/enecajs/seneca-transport/master/README.md"
+    let readmeURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/README.md"
+    let packageURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/package.json"
+
+    const readmeRaw = await Fetch(readmeURL)
+    const readme = await readmeRaw.text()
+    // console.log(readme)
+
+    const packageRaw = await Fetch(packageURL)
+    const packageText = await packageRaw.text()
+    const package = JSON.parse(packageText)
+    // console.log(package)
+}
+
+doDownload()
