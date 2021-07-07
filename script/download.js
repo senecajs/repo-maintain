b3dcbe92f6433e05f185dcdf2c1532b75e719311
Ríona Stokes <rlsted123@gmail.com>
2021-07-07 08:33:29 +0100

    Isolation of org/repo from json

--- script/download.js
+++ script/download.js
@@ -5,22 +5,22 @@ const Plugins = require('../data/json/plugins.json')
 async function doDownload() {
     console.log("Download function initiated.")
 
-    list = JSON.parse(Plugins)
-    list.forEach(repo => {
-        console.log(repo.key())
-    });
+    console.log(Plugins)
+    objKeys = Object.keys(Plugins)
+    console.log(objKeys)
+    
 
     // let readmeURL = "https://raw.githubusercontent.com/enecajs/seneca-transport/master/README.md"
-    let readmeURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/README.md"
-    let packageURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/package.json"
+    // let readmeURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/README.md"
+    // let packageURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/package.json"
 
-    const readmeRaw = await Fetch(readmeURL)
-    const readme = await readmeRaw.text()
+    // const readmeRaw = await Fetch(readmeURL)
+    // const readme = await readmeRaw.text()
     // console.log(readme)
 
-    const packageRaw = await Fetch(packageURL)
-    const packageText = await packageRaw.text()
-    const package = JSON.parse(packageText)
+    // const packageRaw = await Fetch(packageURL)
+    // const packageText = await packageRaw.text()
+    // const package = JSON.parse(packageText)
     // console.log(package)
 }
 
