c6175aaf62076471f9cef3f66df84e44592ea470
Ríona Stokes <rlsted123@gmail.com>
2021-07-07 17:15:25 +0100

    Tests for reading files

--- script/tests/foo.js
+++ script/tests/foo.js
@@ -1,3 +1,4 @@
-let a = {b:1}
-a = undefined
-console.log(a.b)
+Fs = require('fs')
+
+console.log("Empty file:",Fs.readFileSync('../../data/txt/empty.txt', 'utf8'))
+console.log("Two-line file:",Fs.readFileSync('../../data/txt/2lines.txt', 'utf8'))
