b8ba4b058ae14dda66f3fd9b5dbf9afcb876e50f
Ríona Stokes <rlsted123@gmail.com>
2021-07-07 18:31:02 +0100

    checks.js = initial creation of plugins json data

--- script/checks.js
+++ script/checks.js
@@ -16,7 +16,10 @@
  * -- flags/marks to be in json format --
  */
 
+const Fs = require('fs')
 const Filehound = require('filehound');
+const Path = require('path')
+const jsonFile = require('jsonfile')
 
 const exts = ['md','json']
 const files = Filehound.create()
@@ -25,8 +28,40 @@ const files = Filehound.create()
     .find();
 
 async function doChecks() {
-    // how can I access these outside of this single promise ?
-    files.then(console.log)
+    jsonObj = {}
+    const filePath = await files
+    filePath.forEach(file => {
+        obj = {}
+
+        // name of org/repo
+        const orgRepo = Path.basename(Path.dirname(file)).replace('_','/')
+        obj["org/repo"] = orgRepo
+
+        // github url
+        obj["url"] = "https://github.com/"+orgRepo
+
+        // is file found ?
+        const content = Fs.readFileSync(file, 'utf8')
+        if ("404: Not Found" == content) {
+            obj["status"] = 404
+            obj["content"] = "null"
+        } else {
+            obj["status"] = 200
+            if (0 == content.length) {
+                obj["content"] = "zero_char"
+            } else {
+                obj["content"] = "nonzero_char"
+            }
+        }
+
+        // add to top-level object
+        jsonObj[orgRepo] = obj
+        console.log(obj)
+    });
+
+    var json = Object.values(jsonObj)
+    jsonFile.writeFileSync("../data/json/pluginChecks.json", json, {flag: 'a', EOL: ',', finalEOL: false})
+    console.log("All check results formatted as JSON data. See pluginChecks.json for details.")
 }
 
 doChecks()
