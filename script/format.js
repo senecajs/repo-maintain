436b4a5dd86c78abaa5373c969dd208761082536
Ríona Stokes <rlsted123@gmail.com>
2021-07-07 18:31:42 +0100

    Mod to format.js to see checks data as csv

--- script/format.js
+++ script/format.js
@@ -2,16 +2,32 @@
 const Fs = require('fs')
 const Export = require ('jsonexport')
 
-const Input = Fs.createReadStream("../data/json/results.json")
-const Output = Fs.createWriteStream("../data/csv/results.csv")
+// GITHUB SEARCH API RESULTS
+// const Input = Fs.createReadStream("../data/json/results.json")
+// const Output = Fs.createWriteStream("../data/csv/results.csv")
+
+// // check if results.csv exists, and if so, clear it
+// let results = "../data/csv/results.csv"
+// if(Fs.existsSync(results)){
+//   Fs.unlinkSync(results)
+//   console.log("Previous results.csv file deleted.")
+// }
+
+// // create csv file from json file
+// Input.pipe(Export()).pipe(Output)
+// console.log("JSON data successfully reformatted as CSV. See results.csv for data.")
+
+// PLUGIN CHECKS
+const Input = Fs.createReadStream("../data/json/pluginChecks.json")
+const Output = Fs.createWriteStream("../data/csv/pluginChecks.csv")
 
 // check if results.csv exists, and if so, clear it
-let results = "../data/csv/results.csv"
+let results = "../data/csv/pluginChecks.csv"
 if(Fs.existsSync(results)){
   Fs.unlinkSync(results)
-  console.log("Previous results.csv file deleted.")
+  console.log("Previous pluginChecks.csv file deleted.")
 }
 
 // create csv file from json file
 Input.pipe(Export()).pipe(Output)
-console.log("JSON data successfully reformatted as CSV. See results.csv for data.")
+console.log("JSON data successfully reformatted as CSV. See pluginChecks.csv for data.")
