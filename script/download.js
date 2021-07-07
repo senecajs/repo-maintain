572673bde5848afe1ac864cc8b722bdba5bbb3a6
Ríona Stokes <rlsted123@gmail.com>
2021-07-07 09:38:18 +0100

    README + package => unique org/repo paths

--- script/download.js
+++ script/download.js
@@ -1,27 +1,38 @@
 const Fetch = require('node-fetch')
-const Fs = require('fs')
+const Fs = require('fs-extra')
+const Clear = require('clear-dir')
 const Plugins = require('../data/json/plugins.json')
 
 async function doDownload() {
     console.log("Download function initiated.")
 
-    console.log(Plugins)
     objKeys = Object.keys(Plugins)
-    console.log(objKeys)
+    // console.log(objKeys)
+    for (let i = 0; i < objKeys.length; i++) {
+
+        
+        let readmeURL = "https://raw.githubusercontent.com/" + objKeys[i] + "/master/README.md"
+        let packageURL = "https://raw.githubusercontent.com/" + objKeys[i] + "/master/package.json"
+
+        // make sure directory exists and if so, clear it
+        Fs.emptyDirSync('../data/downloads/'+objKeys[i])
+        console.log("Previous", objKeys[i], "directory cleared.")
+        
+
+        const readmeRaw = await Fetch(readmeURL)
+        const readme = await readmeRaw.text()
+        Fs.writeFileSync('../data/downloads/'+objKeys[i]+'/README.md', readme)
+        console.log("README.md created.")
+
+        const packageRaw = await Fetch(packageURL)
+        const packageText = await packageRaw.text()
+        const package = JSON.stringify(packageText)
+        Fs.writeFileSync('../data/downloads/'+objKeys[i]+'/package.json', package)
+        console.log("package.json created.")
+    }
     
 
-    // let readmeURL = "https://raw.githubusercontent.com/enecajs/seneca-transport/master/README.md"
-    // let readmeURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/README.md"
-    // let packageURL = "https://raw.githubusercontent.com/" + orgRepo + "/master/package.json"
-
-    // const readmeRaw = await Fetch(readmeURL)
-    // const readme = await readmeRaw.text()
-    // console.log(readme)
-
-    // const packageRaw = await Fetch(packageURL)
-    // const packageText = await packageRaw.text()
-    // const package = JSON.parse(packageText)
-    // console.log(package)
+    
 }
 
 doDownload()
