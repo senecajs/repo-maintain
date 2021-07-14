b35156502fd62797b5620e42f6780ce1c563846c
Ríona Stokes <rlsted123@gmail.com>
2021-07-14 14:37:54 +0100

    Wednesday meeting small changes

--- script/plugins.js
+++ script/plugins.js
@@ -1,24 +1,19 @@
 const Fs = require('fs')
 
-// check if plugins.json exists, and if so, clear it
-let data = "../data/json/plugins.json"
-if(Fs.existsSync(data)){
-  Fs.unlinkSync(data)
-  console.log("Previous plugins.json file deleted.")
-}
+// idempotency
 
 plugins = {
-    "senecajs/seneca-transport" : {"kind":"plugin"},
-    "Istar-Eldritch/seneca-sequelize" : {"kind":"plugin"},
-    "senecajs-attic/seneca-pubsub" : {"kind":"plugin"},
-    "chico/seneca-perm-editor" : {"kind":"plugin"},
-    "timugz/seneca-token" : {"kind":"plugin"},
-    "voxgig/seneca-promisify" : {"kind":"plugin"},
-    "senecajs/seneca-parambulator" : {"kind":"plugin"},
-    "rjrodger/seneca-todo-flex" : {"kind":"plugin"},
-    "senecajs-labs/seneca-import-csv" : {"kind":"plugin"},
-    "GlenTiki/seneca-load-balancer" : {"kind":"plugin"},
-    "salmanm/seneca-calendar-flex" : {"kind":"plugin"}
+  "senecajs/seneca-transport" : {"kind":"plugin"},
+  "Istar-Eldritch/seneca-sequelize" : {"kind":"plugin"},
+  "senecajs-attic/seneca-pubsub" : {"kind":"plugin"},
+  "chico/seneca-perm-editor" : {"kind":"plugin"},
+  "timugz/seneca-token" : {"kind":"plugin"},
+  "voxgig/seneca-promisify" : {"kind":"plugin"},
+  "senecajs/seneca-parambulator" : {"kind":"plugin"},
+  "rjrodger/seneca-todo-flex" : {"kind":"plugin"},
+  "senecajs-labs/seneca-import-csv" : {"kind":"plugin"},
+  "GlenTiki/seneca-load-balancer" : {"kind":"plugin"},
+  "salmanm/seneca-calendar-flex" : {"kind":"plugin"}
 }
 
 json = JSON.stringify(plugins)
