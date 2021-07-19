75526a245672095e3b6f2dddd057305a3381f51c
Ríona Stokes <rlsted123@gmail.com>
2021-07-19 11:42:34 +0100

    Basic small updates to download.js file

--- script/format.js
+++ script/format.js
@@ -6,13 +6,6 @@ const Export = require ('jsonexport')
 // const Input = Fs.createReadStream("../data/json/results.json")
 // const Output = Fs.createWriteStream("../data/csv/results.csv")
 
-// // check if results.csv exists, and if so, clear it
-// let results = "../data/csv/results.csv"
-// if(Fs.existsSync(results)){
-//   Fs.unlinkSync(results)
-//   console.log("Previous results.csv file deleted.")
-// }
-
 // // create csv file from json file
 // Input.pipe(Export()).pipe(Output)
 // console.log("JSON data successfully reformatted as CSV. See results.csv for data.")
@@ -20,13 +13,6 @@ const Export = require ('jsonexport')
 // PLUGIN CHECKS
 const Input = Fs.createReadStream("../data/json/pluginChecks.json")
 const Output = Fs.createWriteStream("../data/csv/pluginChecks.csv")
-
-// check if results.csv exists, and if so, clear it
-let checks = "../data/csv/pluginChecks.csv"
-if(Fs.existsSync(checks)){
-  Fs.unlinkSync(checks)
-  console.log("Previous pluginChecks.csv file deleted.")
-}
 
 // create csv file from json file
 Input.pipe(Export()).pipe(Output)
