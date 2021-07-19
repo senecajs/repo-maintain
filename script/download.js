666e3b88b6db0f2332c3a1bc66733080706b142e
Ríona Stokes <rlsted123@gmail.com>
2021-07-19 11:42:34 +0100

    Basic small updates to download.js file

--- script/download.js
+++ script/download.js
@@ -1,3 +1,4 @@
+const Chalk = require('chalk')
 const Fetch = require('node-fetch')
 const Fs = require('fs-extra')
 const Plugins = require('../data/json/plugins.json')
@@ -13,11 +14,6 @@ async function doDownloadPlugins() {
 
         // change / to __ in objKeys[i]
         const orgRepo = objKeys[i].replace('/','__')
-        
-        // use regexp to match any README version (uppercase, lowercase, mix of both)
-        // would this even work?? 
-        // let readmeREGEX = foo
-        // add readmeREGEX into url string
 
         // below URLs are valid even if master branch is named "main"
         // become function in future
@@ -27,49 +23,9 @@ async function doDownloadPlugins() {
         // make sure directory exists and if so, clear it
         // BY HAND !!
 
-        const readmeRaw = await Fetch(readmeURL)
-        // check for 200 status before proceeding
-        let readme = await readmeRaw.text()
-        // re-fetch using readme.md instead of README.md if latter returns 404 error
-        // if ("404: Not Found" == readme) {
-        //     let newURL = "https://raw.githubusercontent.com/" + objKeys[i] + "/master/readme.md"
-        //     const newRaw = await Fetch(newURL)
-        //     readme = await newRaw.text()
-        // }
-        Fs.writeFileSync('../data/downloads/'+orgRepo+'/README.md', readme)
-        console.log("README.md created.")
-
-        const packageRaw = await Fetch(packageURL)
-        const package = await packageRaw.text()
-        // just stringifying a string
-        // const package = JSON.stringify(packageText)
-        Fs.writeFileSync('../data/downloads/'+orgRepo+'/package.json', package)
-        console.log("package.json created.")
-    }
-    
-}
-
-
-// Download pausing around loop 500-700, use console log to periodically find status of running script?
-async function doDownloadResults() {
-    console.log("Download function initiated for results.json.")
-
-    const json = await Fs.readJson('../data/json/results.json')
-
-    for (let i = 0; i < 700; i++) {
-
-        let repo = json[i]
-        // change / to _ in json[i]
-        const orgRepo = repo.full_name.replace('/','_')
-        
-        // below URLs are valid even if master branch is named "main"
-        let readmeURL = "https://raw.githubusercontent.com/" + repo.full_name + "/master/README.md"
-        let packageURL = "https://raw.githubusercontent.com/" + repo.full_name + "/master/package.json"
-
-        // make sure directory exists and if so, clear it
-        Fs.emptyDirSync('../data/downloads/'+orgRepo)
-        console.log("[",i,"] Previous", orgRepo, "directory cleared.")
-        
+        // wait to new directory creation, and write files to that
+        const createDir = await Fs.ensureDir('../data/downloads/'+orgRepo)
+        console.log(Chalk.cyan(objKeys[i]))
 
         const readmeRaw = await Fetch(readmeURL)
         let readme = await readmeRaw.text()
@@ -77,13 +33,12 @@ async function doDownloadResults() {
         console.log("README.md created.")
 
         const packageRaw = await Fetch(packageURL)
-        const packageText = await packageRaw.text()
-        const package = JSON.stringify(packageText)
+        let package = await packageRaw.text()
         Fs.writeFileSync('../data/downloads/'+orgRepo+'/package.json', package)
         console.log("package.json created.")
     }
-    console.log("Downloads complete.")
 }
 
+// if required, find doDownloadResults function in github commits
 
 doDownloadPlugins()
