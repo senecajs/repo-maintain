ab70f5dd8ab84e90470a406f5b00ca52bb70b1fd
Ríona Stokes <rlsted123@gmail.com>
2021-07-22 12:18:30 +0100

    Full comments update and rename for runChecks.js

--- script/runChecks.js
+++ script/runChecks.js
@@ -7,12 +7,19 @@ const jsonFile = require('jsonfile') // easily handle JSON files
 const _ = require('underscore') // for filtering of data
 
 // file-related constants
-const checkList = require('../design/checks/checks.js')
-const error404 = Fs.readFileSync('../data/txt/404.txt')
+const checkList = require('../design/checks/checks.js') // extensible format for plugin checks
+const error404 = Fs.readFileSync('../data/txt/404.txt') // file not found custom error message
 
 // function-related constants
 const checkOps = checkOperations()
-//------------------------------------------------------------------
+//---------------------------------------------------------------
+/**
+ * ORDER OF OPERATIONS
+ * 
+ * run() => runChecks() => checkOperations()
+ *                          ^
+ *                          this is run for every check, for every plugin
+ */
 
 
 // this creates a promise object
@@ -21,13 +28,14 @@ const plugins = Filehound.create()
     .directory()
     .find();
 
+// main function
 async function runChecks() {
     const pluginList = await plugins
     for (let i = 0; i < pluginList.length; i++) {
         let plugin =  pluginList[i]
         
         results = []
-
+        // relative path towards downloaded files - plugin specific
         const pluginRelPath = Path.basename(plugin)
         const orgRepo = pluginRelPath.replace('__','/')
         console.log("\n\n",Chalk.blue(orgRepo))
@@ -37,14 +45,15 @@ async function runChecks() {
             let checkDetails = checkList[checkName]
             checkDetails.name = checkName
 
-            // if check taken from checks.js does not exist in checkOperations,
-            // print error message and proceed to next check in list
+            // make sure operation of function is detailed below
             let checkKind = checkOps[checkDetails.kind]
             if(null == checkKind) {
                 console.log('WARNING', 'Check does not exist', checkName, checkDetails.kind)
+                // proceed to next check
                 continue
             }
 
+            // run each of the checks for each plugin print to console
             let res = await checkKind(checkDetails, pluginRelPath)
             console.log(Chalk.cyan("\nCheck:"),res)
             results.push(res)
@@ -52,12 +61,15 @@ async function runChecks() {
     }
 }
 
+// --------------------------------------------------------------------
 async function run() {
     console.log(Chalk.bold("\nAll Checks:"))
     let checkResults = await runChecks()
     console.log(Chalk.bold("\nChecks complete.\n"))
 }
+// --------------------------------------------------------------------
 
+// what to do for each check is detailed here
 function checkOperations() {
     return {
         file_exist: async function(checkDetails, pluginRelPath) {
@@ -65,6 +77,7 @@ function checkOperations() {
             let file = checkDetails.file
             let filePath = '../data/downloads/'+pluginRelPath+'/'+file
             const fileContent = Fs.readFileSync(filePath)
+            // check if "content of file" is just custom 404 error message
             if (error404 == fileContent) {
                 var status = false
                 var status_code = "404 File not found"
@@ -83,10 +96,12 @@ function checkOperations() {
         },
 
         content_contain: async function(checkDetails, pluginRelPath) {
+
             let file = checkDetails.file
             let searchContent = checkDetails.contains
             let filePath = '../data/downloads/'+pluginRelPath+'/'+file
             const fileContent = Fs.readFileSync(filePath)
+
             if (fileContent.includes(searchContent)) {
                 var status = true
                 var status_code = "200 Search string found."
@@ -106,18 +121,6 @@ function checkOperations() {
     }
 }
 
+// --------------------------------------------------------------------
 run()
-
-/**
- * TASK ORDER
- * 
- * For each directory of downloaded files (named for plugin):
- *      For each check in checks.js:
- *          Read "kind", run associated task in makeCheckFuncs function
- *          Return result of task and write to object
- *      Proceed to next task
- * Proceed to next repo.
- * 
- */
-
-
+// --------------------------------------------------------------------
