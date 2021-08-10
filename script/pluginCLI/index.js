1ade40383b6dda7eecba4d4f9cdd03caef4e041b
Ríona Stokes <rlsted123@gmail.com>
2021-08-10 13:19:38 +0100

    CLI taking user input

--- script/pluginCLI/index.js
+++ script/pluginCLI/index.js
@@ -1,27 +1,19 @@
 #! /usr/bin/env node
 
-// console.log("Thanks for creating a plugin")
-// console.log(process.argv)
+const readLine = require('readline')
+// const Chalk = require('chalk')
 
-// const [ nodeExec, scriptPath, command, ...names] = process.argv
-// const thanks = "Thanks for creating a plugin"
-// // console.log(names)
-// if (command === '--by') {
-//     console.log(thanks,names.join(', '))
-// } else {
-//     console.log(thanks)
-// }
+const Rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-const [ nodeExec, scriptPath, command, names] = process.argv
-const thanks = "Thanks for creating a plugin"
-if (command === '--author') {
-    console.log(thanks,names,"!")
-} else {
-    console.log(thanks,"!")
-}
-// it's not taking the two into account
-if (command === '--plugin') {
-    console.log("Time to test",names)
-} else {
-    console.log("Time to test your plugin")
-}
+Rl.question("What is your name? ", function(name) {
+    Rl.question("And the name of your plugin? ", function(plugin) {
+        console.log(`\nHi ${name}, thanks for creating ${plugin}! Welcome aboard the Seneca team.`)
+        Rl.close()
+    })
+})
+Rl.on("close", function() {
+    process.exit(0)
+})
