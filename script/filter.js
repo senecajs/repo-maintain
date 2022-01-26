29f82eada53a6fd887321dfd61d647c9cbc4ef02
stokesriona <rlsted123@gmail.com>
2022-01-26 22:48:23 +0100

    Filter script removing college mentions

--- script/filter.js
+++ script/filter.js
@@ -1,22 +1,35 @@
 const Fs = require('fs')
-const _ = require('underscore')
 
-const results = require('../data/json/results.json')
+const results = require('../data/json/searchResults.json')
 
 let filter = []
 
 async function doFilter() {
-  console.log("ITEMS TO FILTER : ", results.length)
-  let filterList = _.where(results, {name : "seneca"})
+  console.log('ITEMS TO FILTER : ', results.length)
+  let filterList = results
+  let collegeDash = /-college/i
+  let collegeUnderscore = /_college/i
+  let collegeDesc = /college/i
+  let uniDesc = /university/i
 
-  filterList.forEach(item => {
-    filter.push(item)
-  });
+  filterList.forEach((item) => {
+    if (
+      collegeDash.test(item.name) |
+      collegeUnderscore.test(item.name) |
+      collegeDesc.test(item.description) |
+      uniDesc.test(item.description)
+    ) {
+      console.log(item.name)
+    } else {
+      filter.push(item)
+    }
+  })
 
-  Fs.writeFileSync("../data/json/filter.json", JSON.stringify(filter))
+  Fs.writeFileSync('./data/json/filter.json', JSON.stringify(filter))
 
-  console.log("FILTERED ITEMS : ", filter.length)
-  
+  console.log('ITEMS TO FILTER : ', results.length)
+  console.log('FILTERED ITEMS : ', filter.length)
+  console.log('REMOVED : ', results.length - filter.length)
 }
 
 doFilter()
