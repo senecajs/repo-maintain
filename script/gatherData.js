d239f096e634b382d3fa346bea7d705759c31984
stokesriona <rlsted123@gmail.com>
2022-02-02 01:47:43 +0100

    gatherData as module.exports

--- script/gatherData.js
+++ script/gatherData.js
@@ -1,7 +1,35 @@
 module.exports = {
-  gatherData: function () {
-    repoData = {}
-    // gather data from API json and download required file content
-    return repoData
+  gatherData: function (apiData, filesReq) {
+    // Node modules
+    const Fs = require('fs')
+
+    // External modules
+    const Fetch = require('node-fetch') // v3 only supports ESM - switch?
+
+    reqData = {}
+    reqData.full_name = apiData.full_name
+    reqData.html_url = apiData.html_url
+    reqData.language = apiData.language
+    reqData.default_branch = apiData.default_branch
+    reqData.forks_count = apiData.forks_count
+    reqData.stargazers_count = apiData.stargazers_count
+    reqData.open_issues = apiData.open_issues
+
+    for (let i = 0; i < filesReq; i++) {
+      let fileName = filesReq[i]
+      let url =
+        'https://raw.githubusercontent.com/' +
+        apiData.full_name +
+        '/master/' +
+        fileName
+
+      const fileRaw = await Fetch(url)
+      if (fileRaw.ok) {
+        let fileContent = await fileRaw.text()
+        reqData[fileName] = fileContent
+      }
+    }
+
+    return reqData
   },
 }
