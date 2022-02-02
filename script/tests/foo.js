25aa19e7fe29184c4028c7f2281dbbada9705812
stokesriona <rlsted123@gmail.com>
2022-02-02 11:07:22 +0100

    Testing Fetch response

--- script/tests/foo.js
+++ script/tests/foo.js
@@ -1,7 +1,18 @@
-const x = 'mario yahoo'
+const Fetch = require('node-fetch')
 
-if ('mario yahoo' == x) {
-  console.log('itsa me')
-} else {
-  console.log('its nota me :(')
+testFetch()
+async function testFetch() {
+  let url =
+    'https://raw.githubusercontent.com/' +
+    'senecajs/seneca-gateway' +
+    '/master/' +
+    'package.json'
+
+  console.log('URL : ', url)
+  const fileRaw = await Fetch(url)
+  console.log('fileRaw : ', fileRaw)
+  console.log('fileRawOK : ', fileRaw.ok)
+  console.log('fileRawSTATUS : ', fileRaw.status)
+  let fileContent = await fileRaw.text()
+  console.log('fileContent : ', fileContent)
 }
